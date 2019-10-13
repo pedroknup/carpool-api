837f487cf6c19fbba5925546d5411dfdbfaa26fd
Pedro Knup <phknup@gmail.com>
2019-10-13 17:14:01 +0200

    test

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,12 +1,12 @@
-import { Request, Response } from "express";
-import * as jwt from "jsonwebtoken";
-import { getRepository } from "typeorm";
-import { validate } from "class-validator";
+import { Request, Response } from 'express';
+import * as jwt from 'jsonwebtoken';
+import { getRepository } from 'typeorm';
+import { validate } from 'class-validator';
 
-import { User } from "../entity/User";
-import config from "../config/config";
-import { users } from "../entity/users";
-import { removeAtt } from "../util/object";
+import { User } from '../entity/User';
+import config from '../config/config';
+import { users } from '../entity/users';
+import { removeAtt } from '../util/object';
 
 class AuthController {
   static login = async (req: Request, res: Response) => {
@@ -30,14 +30,10 @@ class AuthController {
       res.status(401).send();
       return;
     }
-    user = removeAtt(user, ["password"])
+    user = removeAtt(user, ['password']);
 
     //Sing JWT, valid for 1 hour
-    const token = jwt.sign(
-      { userId: user.id, phone: user.phone },
-      config.jwtSecret,
-      { expiresIn: "1h" }
-    );
+    const token = jwt.sign({ userId: user.id, phone: user.phone }, config.jwtSecret, { expiresIn: '1h' });
 
     //Send the jwt in the response
     res.send({ user, token });
@@ -49,8 +45,10 @@ class AuthController {
 
     //Get parameters from the body
     const { oldPassword, newPassword } = req.body;
+    console.log(oldPassword, newPassword);
     if (!(oldPassword && newPassword)) {
       res.status(400).send();
+      return;
     }
 
     //Get user from the database
@@ -60,10 +58,20 @@ class AuthController {
       user = await userRepository.findOneOrFail(id);
     } catch (id) {
       res.status(401).send();
+      return;
+    }
+    if (user == undefined || user == null) {
+      res.status(401).send();
+      return;
     }
 
-    //Check if old password matchs
-    if (!user.checkIfUnencryptedPasswordIsValid(oldPassword)) {
+    try {
+      //Check if old password matchs
+      if (!user.checkIfUnencryptedPasswordIsValid(oldPassword)) {
+        res.status(401).send();
+        return;
+      }
+    } catch (id) {
       res.status(401).send();
       return;
     }
